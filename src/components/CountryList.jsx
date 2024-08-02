841a66b075a53d7965d400c43e44379622fb0d4f
Ruth Cuellar <ruthcllp@gmail.com>
2024-08-02 12:55:30 +0200

    list scroll working

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import defaultImage from "../assets/default-image.jpg"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/free-mode';
+import { FreeMode, Pagination } from 'swiper/modules';
+import defaultImage from "../assets/default-image.jpg";
 
 const CountryList = ({ countries, updateVisitedStatus, deleteCountry, uploadImage }) => {
     const toVisitCountries = countries.filter(country => !country.visited);
     const visitedCountries = countries.filter(country => country.visited);
 
-    // Functionality to upload picture:
     const handleImageUpload = (event, countryId) => {
         const file = event.target.files[0];
         console.log('File selected:', file);
@@ -13,79 +17,207 @@ const CountryList = ({ countries, updateVisitedStatus, deleteCountry, uploadImag
         uploadImage(file, countryId);
     };
 
-    // Function to handle image click
     const handleImageClick = (countryId) => {
         document.getElementById(`file-input-${countryId}`).click();
     };
 
-  
     return (
         <div>
             <h2>To Visit</h2>
-            <ul>
+            <Swiper
+                breakpoints={{
+                    340: {
+                        slidesPerView: 2,
+                        spaceBetween: 15,
+                    },
+                    700: {
+                        slidesPerView: 3,
+                        spaceBetween: 15,
+                    },
+                    1024: {
+                        slidesPerView: 4,
+                        spaceBetween: 20,
+                    },
+                }}
+                freeMode={true}
+                pagination={{ clickable: true }}
+                modules={[FreeMode, Pagination]}
+                className="max-w-[90%] lg:max-w-[80%]"
+            >
                 {toVisitCountries.map(country => (
-                    <div className="card flex mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
-                    <li key={country.id}>
-                        <h1>{country.country_name}</h1>
-                        
-                        <img 
-                            src={country.image_url || defaultImage} 
-                            alt={country.country_name} 
-                            onClick={() => handleImageClick(country.id)}
-                            style={{cursor:"pointer"}}
-                            
-                        />
-                       
-                        <input 
-                            id={`file-input-${country.id}`} 
-                            type="file" 
-                            onChange={(e) => handleImageUpload(e, country.id)} 
-                            style={{ display: 'none' }} 
-                        />
-                        <div className="button-line">
-                            <button className="btn-primary"
-                            onClick={() => updateVisitedStatus(country.id, true)}>Visited
-                            </button>
-                             <button className="btn-remove" onClick={() => deleteCountry(country.id)}>
-                                <span>remove</span>
-                                <div className="icon">
-                                    <i className="fa fa-remove"></i>
-                                    <i className="fa fa-check"></i>
-                                </div>
-                            </button>
+                    <SwiperSlide key={country.id}>
+                        <div className="card flex flex-col mt-6 text-gray-700 bg-white shadow-md rounded-xl">
+                            <h1>{country.country_name}</h1>
+                            <img 
+                                src={country.image_url || defaultImage} 
+                                alt={country.country_name} 
+                                onClick={() => handleImageClick(country.id)}
+                                style={{ cursor: "pointer", width: '100%', height: 'auto' }}
+                            />
+                            <input 
+                                id={`file-input-${country.id}`} 
+                                type="file" 
+                                onChange={(e) => handleImageUpload(e, country.id)} 
+                                style={{ display: 'none' }} 
+                            />
+                            <div className="button-line mt-4 flex justify-between">
+                                <button className="btn-primary" onClick={() => updateVisitedStatus(country.id, true)}>
+                                    Visited
+                                </button>
+                                <button className="btn-remove" onClick={() => deleteCountry(country.id)}>
+                                    <span>Remove</span>
+                                    <div className="icon">
+                                        <i className="fa fa-remove"></i>
+                                        <i className="fa fa-check"></i>
+                                    </div>
+                                </button>
                             </div>
-                        {/* <button className="btn-remove"
-                            onClick={() => deleteCountry(country.id)}>Delete</button> */}
-                    </li>
-                    </div>
+                        </div>
+                    </SwiperSlide>
                 ))}
-            </ul>
+            </Swiper>
+
             <h2>Visited</h2>
-            <ul>
+            <Swiper
+                breakpoints={{
+                    340: {
+                        slidesPerView: 2,
+                        spaceBetween: 15,
+                    },
+                    700: {
+                        slidesPerView: 3,
+                        spaceBetween: 15,
+                    },
+                    1024: {
+                        slidesPerView: 4,
+                        spaceBetween: 20,
+                    },
+                }}
+                freeMode={true}
+                pagination={{ clickable: true }}
+                modules={[FreeMode, Pagination]}
+                className="max-w-[90%] lg:max-w-[80%]"
+            >
                 {visitedCountries.map(country => (
-                    <div className="card flex mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
-                    <li key={country.id}>
-                        <h1>{country.country_name}</h1>
-                        <img 
-                            src={country.image_url || defaultImage} 
-                            alt={country.country_name} 
-                            onClick={() => handleImageClick(country.id)}
-                            style={{cursor:"pointer"}}
-                        />
-                        <input 
-                            id={`file-input-${country.id}`} 
-                            type="file" 
-                            onChange={(e) => handleImageUpload(e, country.id)} 
-                            style={{ display: 'none' }} 
-                        />
-                        <button className="btn-primary"
-                            onClick={() => updateVisitedStatus(country.id, false)}>Mark as Unvisited</button>
-                    </li>
-                    </div>
+                    <SwiperSlide key={country.id}>
+                        <div className="card flex flex-col mt-6 text-gray-700 bg-white shadow-md rounded-xl">
+                            <h1>{country.country_name}</h1>
+                            <img 
+                                src={country.image_url || defaultImage} 
+                                alt={country.country_name} 
+                                onClick={() => handleImageClick(country.id)}
+                                style={{ cursor: "pointer", width: '100%', height: 'auto' }}
+                            />
+                            <input 
+                                id={`file-input-${country.id}`} 
+                                type="file" 
+                                onChange={(e) => handleImageUpload(e, country.id)} 
+                                style={{ display: 'none' }} 
+                            />
+                            <div className="button-line mt-4">
+                                <button className="btn-primary" onClick={() => updateVisitedStatus(country.id, false)}>
+                                    Mark as Unvisited
+                                </button>
+                            </div>
+                        </div>
+                    </SwiperSlide>
                 ))}
-            </ul>
+            </Swiper>
         </div>
     );
 };
 
 export default CountryList;
+
+
+// import React from 'react';
+// import defaultImage from "../assets/default-image.jpg"
+
+// const CountryList = ({ countries, updateVisitedStatus, deleteCountry, uploadImage }) => {
+//     const toVisitCountries = countries.filter(country => !country.visited);
+//     const visitedCountries = countries.filter(country => country.visited);
+
+//     // Functionality to upload picture:
+//     const handleImageUpload = (event, countryId) => {
+//         const file = event.target.files[0];
+//         console.log('File selected:', file);
+//         console.log('Country ID:', countryId);
+//         uploadImage(file, countryId);
+//     };
+
+//     // Function to handle image click
+//     const handleImageClick = (countryId) => {
+//         document.getElementById(`file-input-${countryId}`).click();
+//     };
+
+  
+//     return (
+//         <div>
+//             <h2>To Visit</h2>
+//             <ul>
+//                 {toVisitCountries.map(country => (
+//                     <div className="card flex mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+//                     <li key={country.id}>
+//                         <h1>{country.country_name}</h1>
+                        
+//                         <img 
+//                             src={country.image_url || defaultImage} 
+//                             alt={country.country_name} 
+//                             onClick={() => handleImageClick(country.id)}
+//                             style={{cursor:"pointer"}}
+                            
+//                         />
+                       
+//                         <input 
+//                             id={`file-input-${country.id}`} 
+//                             type="file" 
+//                             onChange={(e) => handleImageUpload(e, country.id)} 
+//                             style={{ display: 'none' }} 
+//                         />
+//                         <div className="button-line">
+//                             <button className="btn-primary"
+//                             onClick={() => updateVisitedStatus(country.id, true)}>Visited
+//                             </button>
+//                              <button className="btn-remove" onClick={() => deleteCountry(country.id)}>
+//                                 <span>remove</span>
+//                                 <div className="icon">
+//                                     <i className="fa fa-remove"></i>
+//                                     <i className="fa fa-check"></i>
+//                                 </div>
+//                             </button>
+//                             </div>
+//                         {/* <button className="btn-remove"
+//                             onClick={() => deleteCountry(country.id)}>Delete</button> */}
+//                     </li>
+//                     </div>
+//                 ))}
+//             </ul>
+//             <h2>Visited</h2>
+//             <ul>
+//                 {visitedCountries.map(country => (
+//                     <div className="card flex mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96">
+//                     <li key={country.id}>
+//                         <h1>{country.country_name}</h1>
+//                         <img 
+//                             src={country.image_url || defaultImage} 
+//                             alt={country.country_name} 
+//                             onClick={() => handleImageClick(country.id)}
+//                             style={{cursor:"pointer"}}
+//                         />
+//                         <input 
+//                             id={`file-input-${country.id}`} 
+//                             type="file" 
+//                             onChange={(e) => handleImageUpload(e, country.id)} 
+//                             style={{ display: 'none' }} 
+//                         />
+//                         <button className="btn-primary"
+//                             onClick={() => updateVisitedStatus(country.id, false)}>Mark as Unvisited</button>
+//                     </li>
+//                     </div>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// };
+
+// export default CountryList;
